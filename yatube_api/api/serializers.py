1665c9a2ad9e9f33e108da99f86c9e2bb9b08ca7
yeeeyoooduck <yeeeyoooduck@gmail.com>
2025-01-27 21:09:18 +0300

    fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -19,6 +19,7 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username')
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Comment
